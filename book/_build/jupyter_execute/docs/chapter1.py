d88f11ffd52b32a50e34c08863574472c5b103aa
nencarc-digital <nencarc.digital@gmail.com>
2022-12-06 12:01:32 +0000

    December=06

--- book/_build/jupyter_execute/docs/chapter1.py
+++ book/_build/jupyter_execute/docs/chapter1.py
@@ -4,9 +4,13 @@
 # (about_py)=
 # 
 # # Chapter 1
+# 
+# ffffffff
+# yes
 # > Here is a temporary test
 # sdfsdfdsfsdf
 # sdfsdfsdfsdfsdfsd
+# helloo
 # > \"Python has gotten sufficiently weapons grade that we don't descend
 # > into R anymore. Sorry, R people. I used to be one of you but we no
 # > longer descend into R.\" -- Chris Wiggins
